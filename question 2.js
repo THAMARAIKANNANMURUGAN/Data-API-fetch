57cbdf1a4f27c3e3d989b6d2778997af1ba50c25
thamarai-kannan <129540289+THAMARAIKANNANMURUGAN@users.noreply.github.com>
2023-11-29 13:47:28 +0530

    Add files via upload

---
+++ question 2.js
@@ -0,0 +1,48 @@
+{
+    "basics": {
+      "name": "Thamaraikannan",
+      "email": "thamaraikannnan2408@gmail.com",
+      "phone": "A phone number, with any formatting you like. E.g. (555) 555-5555.",
+      "degree": "",
+      "website": "Your website URL",
+      "summary": "A one-sentence to one-paragraph overview text. Do not include any line-breaks.",
+      "location": {
+        "address": "Your street address or mailing address",
+        "postalCode": "Your postal code (ZIP in the U.S.)",
+        "city": "Your city",
+        "region": "Your region (state in the U.S.)"
+      },
+   
+    "education": [
+      {
+        "institution": "Your school name",
+        "area": "Your area of study or degree earned",
+        "studyType": "",
+        "startDate": "Your start date, in YYYY-MM-DD format",
+        "endDate": "Your completion date, in YYYY-MM-DD format",
+        "gpa": "",
+        "courses": [
+          ""
+        ]
+      }
+    ],
+   
+   
+    "skills": [
+      {
+        "name": "A category of job skills ( 'Programming Languages')",
+        "level": "",
+        "keywords": [
+          "Keywords under this category ( 'Java', 'C++', etc)"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Language name",
+        "fluency": "Your language fluency"
+      }
+    ],
+   
+  }
+  
